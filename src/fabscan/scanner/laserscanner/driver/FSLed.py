d46fee38b934b6ac2c8806625f8b856c42192e0e
Dave Astels <dastels@daveastels.com>
2019-05-23 03:40:50 +0100

    first pass at crickit support

--- src/fabscan/scanner/laserscanner/driver/FSLed.py
+++ src/fabscan/scanner/laserscanner/driver/FSLed.py
@@ -6,16 +6,16 @@ __license__ = "GPL v2"
 __maintainer__ = "Mario Lukas"
 __email__ = "info@mariolukas.de"
 import time
+from adafruit_crickit import crickit
+from adafruit_seesaw.neopixel import NeoPixel
 
 class Led(object):
     def __init__(self, serial_object):
-        self.serial_connection = serial_object
+        self.pixels = NeoPixel(crickit.seesaw, 20, 7)
+
 
     def on(self, red, green, blue):
-            command = "M05 R"+str(red)+" G"+str(green)+" B"+str(blue)+";"
-            self.serial_connection.send_and_receive(command)
+        self.pixels.fill((red, green, blue))
 
     def off(self):
-            command = "M05 R0 G0 B0;"
-            self.serial_connection.send_and_receive(command)
-
+        self.pixels.fill((0, 0, 0))
